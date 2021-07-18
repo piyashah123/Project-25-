903a7aa5c6a10b3b3c3f6aadc84e0961846d976c
piyashah123 <82143407+piyashah123@users.noreply.github.com>
2021-07-18 20:07:15 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,17 @@
+class Paper{
+construtor(x,y,r){
+    var option={
+        iesSatatic:false,
+        restitution:0.3,
+    density:1.2
+    }
+    this.body=Bodies.circle (x,y,r,option)
+
+}
+
+
+
+
+
+
+}
